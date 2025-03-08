ab0aaafbaac7b1afd94e5184c566362d773997ed
Rajkumar Verma <Rv782975@gmail.com>
2025-03-08 12:12:00 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let user=0
+let comp=0
+
+const choice=document.querySelectorAll('.choice');
+
+choice.forEach((choice)=>{
+    choice.addEventListener('click',()=>{
+        console.log("choice was clicked")
+    })
+})
